e6b80b72adea7ec37b1f90a4d2b4fdb0554d09e9
Erick Aiken <44817305+ErickAiken@users.noreply.github.com>
2019-10-18 16:03:39 -0700

    Add files via upload

---
+++ Shortest Path/Main_ShortestPath.py
@@ -0,0 +1,21 @@
+
+#This algorithm will try to find the shortest path that visits each point
+import Graph_ShortestPath as Graph
+import Utilities as Utils
+import Algorithm as Optimize
+
+#define where the cities are
+Cities = [[1,5],[2,4],[3,2],[4,3],[5,1], [8,9], [6,6], [4,8]]
+
+#run the simple algorithm
+Optimal_Path = Optimize.Alg(Cities, 1000, 1000, 0)
+print(Optimal_Path)
+
+#fitness of the solution is 1/total_distance_traveled, closer to zero the better
+Fitness = Utils.Fitness(Optimal_Path)
+
+#graph the result
+Graph.plotter("X-Axis", "Y-Axis","Optimal Path", 0, 10, 0, 10, Cities, Optimal_Path, "red")
+print(Utils.Fitness(Optimal_Path))
+
+
